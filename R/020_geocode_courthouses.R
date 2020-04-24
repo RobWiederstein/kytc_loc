dbd9bd829d29b6f79330e418732d436952b6fe6c
Rob Wiederstein <khuon68@gmail.com>
2020-04-24 06:56:02 -0500

    secure key

--- R/020_geocode_courthouses.R
+++ R/020_geocode_courthouses.R
@@ -1,20 +1,18 @@
-#geocode data from google
+# geocode data from google
 library(ggmap)
 file <- "./data_pure/ky_courthouses.csv"
 df.00 <- read.csv(file = file, stringsAsFactors = F)
-# Read in the CSV data and store it in a variable 
+# Read in the CSV data and store it in a variable
 origAddress <- df.00
 
 # Initialize the data frame
 geocoded <- data.frame(stringsAsFactors = FALSE)
 
-# Loop through the addresses to get the latitude and longitude of each address and add it to the
-# origAddress data frame in new columns lat and lon
-#google maps api key = "AIzaSyAY0qNUX6VlevxzOcYcEMh5Cl7Rg7Hs7pY"
-#pass with key=API_KEY
-for(i in 1:nrow(origAddress))
-{
-      # Print("Working...")
+# Loop through the addresses to get the latitude and longitude of each address
+# and add it to the origAddress data frame in new columns lat and lon pass with
+# key=API_KEY
+for (i in 1:nrow(origAddress)) {
+      # Print('Working...')
       result <- geocode(origAddress$addresses[i], output = "latlona", source = "google")
       origAddress$lon[i] <- as.numeric(result[1])
       origAddress$lat[i] <- as.numeric(result[2])
@@ -22,4 +20,4 @@ for(i in 1:nrow(origAddress))
 }
 # Write a CSV file containing origAddress to the working directory
 file <- "./data_pure/courthouses_geocoded.csv"
-write.csv(origAddress, file = file, row.names=FALSE)
+write.csv(origAddress, file = file, row.names = FALSE)
